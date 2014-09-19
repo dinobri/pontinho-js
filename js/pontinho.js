2dc5ae67367979971f1e293833957cc8e7ba03cf
Diego Noura <diego.nobri@gmail.com>
2014-09-19 01:30:17 -0300

    Início da lógico do jogo. Mais animações.

--- js/pontinho.js
+++ js/pontinho.js
@@ -5,6 +5,8 @@ function Jogador(nome, saldo){
 	this.pontRodada = 0;
 	this.pontGeral = 0;
 
+	this.isEstourado = false;
+
 	/*Métodos*/
 	this.atualizaPontuacao = function(valor){
 		if(valor<0){
@@ -36,25 +38,42 @@ app.controller('MesaController', function(){
 	this.novoJogador = new Jogador();
 
 	this.isAdicionando = false;
+	this.isAdicionandoAlerta = false;
+
+	this.isAlertando = false;
 
 	this.alteraPontuacao = function(jogador, valor){
 		var pontuacaoFinal = jogador.pontRodada + valor;
 		if(pontuacaoFinal > 100 || pontuacaoFinal < 0){
+			jogador.pontRodada = (pontuacaoFinal > 100) ? 100 : 0;
 			return false;
 		} else {
 			jogador.pontRodada += valor;
 		}
 	};
 
+	this.habilitaFormNovoJogador = function(){
+		console.log("Teste");
+		this.isAdicionando = true;
+		var input = document.getElementById("novo-nome");
+		console.log(input);
+		input.focus();
+	}
+
 	this.adicionaJogador = function(){
 		if(this.novoJogador.nome && this.novoJogador.saldo){
 			this.jogadores.push(this.novoJogador);
 			this.novoJogador = new Jogador();
-
-			this.isAdicionando = false;
-		} else {
 			this.isAdicionando = false;			
+		} else {
+			this.isAdicionandoAlerta = true;
+			this.cancelaAdicao();
 		}
+	};
+
+	this.cancelaAdicao = function(){
+		this.novoJogador = new Jogador();
+		this.isAdicionando = false
 	};
 
 	this.removeJogador = function(jogador){
@@ -63,7 +82,32 @@ app.controller('MesaController', function(){
 		if(index >= 0){
 			this.jogadores.splice(index, 1);
 		}
+	};
 
+	this.iniciaRodada = function(){
+		var maiorPontuacao = 0;
 
+		// Soma pontuação da rodada a pontGeral	
+		for(var i = 0; i < this.jogadores.length; i++){
+			var jogador = jogadores[i];
+			var pontuacaoFinal = jogador.pontRodada + jogador.pontGeral;
+			if(pontuacaoFinal >= 100){
+				jogador.isEstourado = true;
+			} else {
+				jogador.pontGeral = pontuacaoFinal;
+				
+				//verifica maior pontuação
+				maiorPontuacao = (jogador.pontGeral > maiorPontuacao) ? jogador.pontGeral : maiorPontuacao;
+			}
+			jogador.pontRodada = 0;
+		}
+
+		//verifica maior pontuação
+		for(var i = 0; i < this.jogadores.length; i++){
+			var jogador = jogadores[i];
+			if(jogador.isEstourado){
+				jogador.pontGeral = maiorPontuacao;
+			}
+		}
 	};
 });
